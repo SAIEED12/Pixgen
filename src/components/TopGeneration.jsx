8b339c0e8b32d17a06253bee1850c1c1609dc0a5
SAIEED12 <abusaieed0@gmail.com>
2026-04-29 11:30:57 +0600

    Add new components and update configuration for photo gallery
    - Introduced `Category`, `PhotoCard`, and `TopGeneration` components for displaying photos.
    - Created `AllPhotosPage` and `PhotoDetailsPage` for handling photo listings and details.
    - Updated `next.config.mjs` to include image remote patterns.
    - Added `react-icons` dependency for icon usage.

---
+++ src/components/TopGeneration.jsx
@@ -0,0 +1,21 @@
+import PhotoCard from "./PhotoCard";
+
+const TopGeneration = async () => {
+  const res = await fetch("https://pixgen-green.vercel.app/data.json");
+  const photos = await res.json();
+  const topPhotos = photos.slice(0, 8)
+//   console.log(photos);
+  return (
+    <div>
+      <h1 className="text-2xl font-bold my-5">Top Generations</h1>
+
+      <div className="grid grid-cols-4 gap-5">
+        {topPhotos.map((photo) => (
+          <PhotoCard key={photo.id} photo={photo} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopGeneration;
